74338c6fad4984a4519d0e5d36038d2e1769c761
javigm4 <javierguerreromontero1@gmail.com>
2024-10-31 10:18:54 +0100

    intentar de nuevo

--- main.js
+++ main.js
@@ -25,10 +25,6 @@ const ul = document.querySelector("ul");
 // el textcontent del span es lo que le metamos en pantalla del input
 let nTareas = 0;
 
-const pNoPendingTasks = document.querySelector(
-  "body > div > div-nth-of-type(4) > p"
-);
-
 boton.addEventListener("click", (event) => {
   event.preventDefault();
   let liCreado = document.createElement("li");
