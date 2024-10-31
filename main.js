1433e0ae1b78702ef76e39257842a01f615f6548
javigm4 <javierguerreromontero1@gmail.com>
2024-10-31 09:07:22 +0100

    probando el nth

--- main.js
+++ main.js
@@ -12,5 +12,8 @@ search.classList.add("search");
 const liContainer = document.querySelector("body > div > div.nth.of-type(2)"); //coge el segundo que se llama div
 liContainer.classList.add("li-container");
 
+const taskCount = document.querySelector("body > div > div.nth.of-type(3)");
+taskCount.classList.add("task-count");
+
 /*document.querySelector("body > div > div").classList.add("search");
 document.querySelector("body > div > div.nth.of-type(2)").classList.add("li-container");*/
