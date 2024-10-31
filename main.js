d7d62850caed81bba94c933af825633d44c1e1fb
javigm4 <javierguerreromontero1@gmail.com>
2024-10-31 10:28:24 +0100

    ahora si

--- main.js
+++ main.js
@@ -10,11 +10,11 @@ search.classList.add("search");
 const liContainer = document.querySelector("body > div > div:nth-of-type(3)");
 liContainer.classList.add("li-container");
 
-const taskCount = document.querySelector("body > div > div:nth-of-type(4)");
+const taskEmpty = document.querySelector("body > div > div:nth-of-type(4)");
+taskCount.classList.add("empty");
+
+const taskCount = document.querySelector("body > div > div:nth-of-type(5)");
 taskCount.classList.add("task-count");
-const contadorTareas = document.querySelector(
-  "body > div > div:nth-of-type(4)"
-);
 
 const boton = document.querySelector("body > div > div > form > button");
 boton.classList.add("btn-add");
