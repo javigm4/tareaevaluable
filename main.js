a89942447a4a4f3972f1d1a46f06f893d1221aff
javigm4 <javierguerreromontero1@gmail.com>
2024-10-31 09:06:16 +0100

    le meti el nth.of-type a ver si va

--- main.js
+++ main.js
@@ -3,11 +3,14 @@ import "./style.css";
 /*-----VAMOS A AÑADIR LAS CLASES A LOS ELEMENTOS DEL INDEX.HTML ---*/
 
 /*se añade con .classList.add("")*/
-document.querySelector("body > div").classList.add("container");
+const container = document.querySelector("body > div");
+container.classList.add("container");
 
 const search = document.querySelector("body > div > div");
 search.classList.add("search");
 
+const liContainer = document.querySelector("body > div > div.nth.of-type(2)"); //coge el segundo que se llama div
+liContainer.classList.add("li-container");
+
 /*document.querySelector("body > div > div").classList.add("search");
-document.querySelector("body >")
 document.querySelector("body > div > div.nth.of-type(2)").classList.add("li-container");*/
